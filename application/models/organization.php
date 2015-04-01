1b33d6d12103109bc2b279997c3afc2747328036
faizanayubi <indianayubi@gmail.com>
2015-04-01 23:50:47 +0530

    New Models

---
+++ application/models/organization.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Description of organization
+ *
+ * @author Faizan Ayubi
+ */
+class Organization {
+    //put your code here
+}
---
+++ application/models/review.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Description of review
+ *
+ * @author Faizan Ayubi
+ */
+class Review {
+    //put your code here
+}
